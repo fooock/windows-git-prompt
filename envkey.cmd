5c0553ce094eea58813a9380d4837bd2c9819d3a
Javi <nhitbh@gmail.com>
2018-04-12 01:57:17 +0200

    Added new doskey to check the branch in all git commands

--- envkey.cmd
+++ envkey.cmd
@@ -3,3 +3,4 @@
 :: Create an alias for cd to try to get the branch of the
 :: current project in case of the user is in a git repo 
 DOSKEY cd=cd $* ^&^& %~dp0\winrc.cmd
+DOSKEY git=git $* ^&^& %~dp0\winrc.cmd
