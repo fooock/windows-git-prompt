47501106d124e44bbe8a2ee90e93c11bfd3f3686
Javi <nhitbh@gmail.com>
2018-04-12 01:18:37 +0200

    Change script name

--- envkey.cmd
+++ envkey.cmd
@@ -2,4 +2,4 @@
 
 :: Create an alias for cd to try to get the branch of the
 :: current project in case of the user is in a git repo 
-DOSKEY cd=cd $* ^&^& %~dp0\bashrc.cmd
+DOSKEY cd=cd $* ^&^& %~dp0\winrc.cmd
