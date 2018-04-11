da6c8be99d8790188e89de9cc2e330c412a2c75b
Javi <nhitbh@gmail.com>
2018-04-12 00:38:52 +0200

    Detect the git branch if exists and add it the the user promt

--- bashrc.cmd
+++ bashrc.cmd
@@ -1,13 +1,25 @@
 @echo off
 
-SET GIT_BRANCH=git branch | FINDSTR "*"
-IF [%GIT_BRANCH%] == [] ( GOTO :prompt )
+cd %cd%
+set GIT_BRANCH=
 
-REM Skip the first char
-SET FMT_BRANCH=%GIT_BRANCH:~0%
+REM Execute git branch command and redirect stderr to NUL
+REM Note that we need to escape the redirectiond and pipe command 
+REM to make the script work
+FOR /f "tokens=2" %%a in ('git branch 2^> NUL ^| FINDSTR /b "* "') do set GIT_BRANCH=%%a
+IF [%GIT_BRANCH%] == [] ( GOTO :prompt ) ELSE ( GOTO :custom )
+
+:custom
+    SET CUSTOM_PROMPT=^
+$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m^
+$S$C%GIT_BRANCH%$F$_^
+$E[90m$G$E[m$S$E]9;12$E\
+    prompt %CUSTOM_PROMPT%
+    GOTO :eof
 
 :prompt
     SET CUSTOM_PROMPT=^
 $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$_^
 $E[90m$G$E[m$S$E]9;12$E\
     prompt %CUSTOM_PROMPT%
+    GOTO :eof
