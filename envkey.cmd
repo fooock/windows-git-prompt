f7722204b2f4523fdb32a378f0a6a617a20a346b
Javi <nhitbh@gmail.com>
2018-04-15 00:16:03 +0200

    Change promt state if using a python virtual env

--- envkey.cmd
+++ envkey.cmd
@@ -1,6 +1,10 @@
 @echo off
 
-:: Create an alias for cd to try to get the branch of the
+:: Create an alias for cd and git to try to get the branch of the
 :: current project in case of the user is in a git repo 
 DOSKEY cd=cd $* ^&^& %~dp0\winrc.cmd
 DOSKEY git=git $* ^&^& %~dp0\winrc.cmd
+
+:: Aliases for virtual envs
+DOSKEY activate=activate $* ^&^& %~dp0\winrc.cmd
+DOSKEY deactivate=deactivate ^&^& %~dp0\winrc.cmd
--- winrc.cmd
+++ winrc.cmd
@@ -1,27 +1,44 @@
 @echo off
 
-set GIT_BRANCH=
+SET GIT_BRANCH=
+SET CURRENT_ENV=
 
 REM Execute git branch command and redirect stderr to NUL
 REM Note that we need to escape the redirectiond and pipe command 
 REM to make the script work
-FOR /f "tokens=2" %%a in ('git branch 2^> NUL ^| FINDSTR /b "* "') do set GIT_BRANCH=%%a
+FOR /f "tokens=2" %%a in ('git branch 2^> NUL ^| FINDSTR /b "* "') do SET GIT_BRANCH=%%a
 
 :: Detect if the user is currently using anu virtual env
 SET CURRENT_ENV=%CONDA_DEFAULT_ENV%
 IF [%GIT_BRANCH%] == [] ( GOTO :prompt ) ELSE ( GOTO :custom )
 
 :custom
-    SET CUSTOM_PROMPT=^
+    IF [%CURRENT_ENV%] == [] (
+        SET CUSTOM_PROMPT=^
 $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m^
 $S$C%GIT_BRANCH%$F$_^
 $E[90m$G$E[m$S$E]9;12$E\
+    ) ELSE (
+        SET CUSTOM_PROMPT=^
+$C%CURRENT_ENV%$F$S^
+$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m^
+$S$C%GIT_BRANCH%$F$_^
+$E[90m$G$E[m$S$E]9;12$E\
+    )
     prompt %CUSTOM_PROMPT%
     GOTO :eof
 
 :prompt
-    SET CUSTOM_PROMPT=^
+    IF [%CURRENT_ENV%] == [] (
+        SET CUSTOM_PROMPT=^
 $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$_^
 $E[90m$G$E[m$S$E]9;12$E\
+        prompt %CUSTOM_PROMPT%
+    ) ELSE (
+        SET CUSTOM_PROMPT=^
+$C%CURRENT_ENV%$F$S^
+$E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S$E[92m$P$E[90m$_^
+$E[90m$G$E[m$S$E]9;12$E\
+    )
     prompt %CUSTOM_PROMPT%
     GOTO :eof
